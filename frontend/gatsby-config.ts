16e090ea8c53ea1fe60be5954c6369e52795a84d
Mark Sze <mark@sze.family>
2024-11-16 11:30:50 +1100

    References to AG2 updated

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -16,7 +16,7 @@ require("dotenv").config({
 const config: GatsbyConfig = {
   pathPrefix: `${process.env.PREFIX_PATH_VALUE}`,
   siteMetadata: {
-    title: `AutoGen Studio [Beta]`,
+    title: `AG2 Studio [Beta]`,
     description: `Build Multi-Agent Apps`,
     siteUrl: `http://tbd.place`,
   },
