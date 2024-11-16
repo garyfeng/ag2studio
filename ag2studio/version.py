be07f59f2ad80d61d31edb973f1ac90a2726eeda
Mark Sze <mark@sze.family>
2024-11-16 11:09:53 +1100

    Load base AG2Studio files

---
+++ ag2studio/version.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023 - 2024, Owners of https://github.com/ag2ai
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Portions derived from  https://github.com/microsoft/autogen are under the MIT License.
+# SPDX-License-Identifier: MIT
+VERSION = "0.0.01rc1"
+__version__ = VERSION
+APP_NAME = "ag2studio"
